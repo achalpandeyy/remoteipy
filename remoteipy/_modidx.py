58734eaf8725291582f2e6f9b7648ecdbb09b984
achal <pandeyachal.ap@gmail.com>
2025-12-10 18:37:38 +0000

    api changes and add some docs; remove all "running" code cells because they need a remote machine to connect to

--- remoteipy/_modidx.py
+++ remoteipy/_modidx.py
@@ -8,9 +8,6 @@ d = { 'settings': { 'branch': 'main',
   'syms': { 'remoteipy.core': { 'remoteipy.core.Client': ('core.html#client', 'remoteipy/core.py'),
                                 'remoteipy.core.connect': ('core.html#connect', 'remoteipy/core.py'),
                                 'remoteipy.core.disconnect': ('core.html#disconnect', 'remoteipy/core.py'),
-                                'remoteipy.core.run_remote_blocking': ('core.html#run_remote_blocking', 'remoteipy/core.py')},
-            'remoteipy.remoteipy': { 'remoteipy.remoteipy.Client': ('remoteipy.html#client', 'remoteipy/remoteipy.py'),
-                                     'remoteipy.remoteipy.connect': ('remoteipy.html#connect', 'remoteipy/remoteipy.py'),
-                                     'remoteipy.remoteipy.disconnect': ('remoteipy.html#disconnect', 'remoteipy/remoteipy.py'),
-                                     'remoteipy.remoteipy.run_remote_blocking': ( 'remoteipy.html#run_remote_blocking',
-                                                                                  'remoteipy/remoteipy.py')}}}
+                                'remoteipy.core.is_connected': ('core.html#is_connected', 'remoteipy/core.py'),
+                                'remoteipy.core.remote_python': ('core.html#remote_python', 'remoteipy/core.py'),
+                                'remoteipy.core.run_remote_blocking': ('core.html#run_remote_blocking', 'remoteipy/core.py')}}}
